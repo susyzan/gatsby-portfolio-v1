7f3233ca863e450e54c7629b9451484695ed81da
Susanna Zanatta <susanna.zanatta@gmail.com>
2020-11-24 20:13:06 +0000

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
     {
       resolve: 'gatsby-v2-plugin-page-transitions',
       options: {
-        transitionTime: 500
+        transitionTime: 800
       }
     },
     {
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,6 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
-import PageTransition from 'gatsby-v2-plugin-page-transitions'
 
 import "../styles/reset.css"
 import "../styles/accessibility.css"
@@ -13,13 +11,9 @@ import SEO from "../components/seo"
 const Work = () => (
   <Layout>
     <SEO title="Susanna Zanatta - Home" />
-      <PageTransition>
         <Section id="banner" bg="bg_tertiary">
             <h1>Work</h1>
         </Section>
-        <Link to="/page-2/">Go to page 2</Link> <br />
-        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-      </PageTransition>
   </Layout>
 )
 
